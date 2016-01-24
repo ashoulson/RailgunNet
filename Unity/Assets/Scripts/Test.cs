75d4d8470699dcee58b61878d69679b76725ddae
ashoulson <ashoulson@gmail.com>
2016-01-23 21:55:48 -0500

    Adding BitPacker class

---
+++ Unity/Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using RailgunNet;
+
+public class Test : MonoBehaviour 
+{
+  void Start () 
+  {
+    bool failed = false;
+    for (int i = 0; i < 50; i++)
+    {
+      if (BitPacker.TestBitPacker(100, 500) == false)
+        failed = true;
+    }
+    Debug.Log(failed);
+  }
+  
+  void Update () 
+  {
+  }
+}
