429b3946f5e195fa61e30203a6ae20d05bc64d80
Alexander Shoulson <ashoulson@gmail.com>
2016-01-24 01:51:04 -0500

    Starting on EntityState, adding Encoder Library

---
+++ RailgunNet/EntityState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Railgun
+{
+  public struct EntityState
+  {
+    #region Flags
+    private const uint  FLAG_USER_ID        = 0x01;
+    private const uint  FLAG_ENTITY_ID      = 0x02;
+    private const uint  FLAG_ARCHETYPE_ID   = 0x04;
+    private const uint  FLAG_X              = 0x08;
+    private const uint  FLAG_Y              = 0x10;
+    private const uint  FLAG_ANGLE          = 0x20;
+    private const uint  FLAG_STATUS         = 0x40;
+
+    internal const uint FLAG_ALL            = 0x7F;
+    #endregion
+
+    private int userId;
+    private int entityId;
+    private int archetypeId;
+
+    private float x;
+    private float y;
+    private float angle;
+
+    private uint status;
+  }
+}
---
+++ RailgunNet/Serialization/DefaultEncoders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Railgun
+{
+  internal static class DefaultEncoders
+  {
+    internal static IntEncoder   EntityFlagEncoder  = null;
+
+    internal static IntEncoder   UserIdEncoder      = null;
+    internal static IntEncoder   EntityIdEncoder    = null;
+    internal static IntEncoder   ArchetypeIdEncoder = null;
+    internal static IntEncoder   StatusEncoder      = null;
+
+    internal static FloatEncoder CoordinateEncoder  = null;
+    internal static FloatEncoder AngleEncoder       = null;
+
+    internal static void Initialize()
+    {
+      DefaultEncoders.EntityFlagEncoder  = new IntEncoder(0, (int)EntityState.FLAG_ALL);
+
+      DefaultEncoders.UserIdEncoder      = new IntEncoder(0, 4095);
+      DefaultEncoders.EntityIdEncoder    = new IntEncoder(0, 65535);
+      DefaultEncoders.ArchetypeIdEncoder = new IntEncoder(0, 255);
+      DefaultEncoders.StatusEncoder      = new IntEncoder(0, 0xFFF);
+
+      DefaultEncoders.AngleEncoder       = new FloatEncoder(0.0f, 360.0f, 0.1f);
+      DefaultEncoders.CoordinateEncoder  = new FloatEncoder(-2048.0f, 2048.0f, 0.01f);
+    }
+  }
+}
