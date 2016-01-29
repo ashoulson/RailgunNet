7a37d1baa24d8b2b8dc1ae24162a7a40bd89430b
Alexander Shoulson <ashoulson@gmail.com>
2016-01-28 21:01:51 -0500

    Throwing ideas around, setting a save point

---
+++ RailgunNet/New/Entity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Reservoir;
+
+namespace Railgun
+{
+  public class Entity : Poolable<Entity>
+  {
+    public byte Type { get; internal set; }
+
+    public NodeList<Box> boxes;
+    public Dictionary<byte, Box> typeToBox;
+
+    public Entity()
+    {
+      this.boxes = new NodeList<Box>();
+      this.typeToBox = new Dictionary<byte, Box>();
+    }
+
+    protected override void Reset()
+    {
+      foreach (Box box in this.boxes)
+        box.Free();
+      this.typeToBox.Clear();
+    }
+
+    /// <summary>
+    /// Deep-copies this entity into the given image.
+    /// </summary>
+    internal void Populate(Image image)
+    {
+      foreach (Box box in this.boxes)
+        image.Add(box.Clone());
+    }
+  }
+}
