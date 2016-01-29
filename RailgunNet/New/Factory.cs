f1e0db6e54c8d07d24b0b554540ed69c26aba922
ashoulson <ashoulson@gmail.com>
2016-01-28 21:01:51 -0500

    Throwing ideas around, setting a save point

---
+++ RailgunNet/New/Factory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Reservoir;
+
+namespace Railgun
+{
+  /// <summary>
+  /// Factories are responsible for creating boxes.
+  /// </summary>
+  public abstract class Factory
+  {
+    internal NodeList<Box> freeList;
+
+    public Factory()
+    {
+      this.freeList = new NodeList<Box>();
+    }
+
+    internal abstract Box Allocate();
+
+    internal void Deallocate(Box box)
+    {
+      if (box.Factory != this)
+        throw new ArgumentException("Box must be from this factory");
+
+      box.Reset();
+      this.freeList.Add(box);
+    }
+  }
+
+  /// <summary>
+  /// Factories are responsible for creating boxes. For a given box class,
+  /// you will need to instantiate and provide a typed box factory in order
+  /// to provide the system with a way to create boxes of your type.
+  /// 
+  /// This is a pooled data structure. Unused boxes are freed and returned.
+  /// </summary>
+  public sealed class Factory<T> : Factory
+    where T : Box<T>, new()
+  {
+    internal override Box Allocate()
+    {
+      Box box = null;
+      if (this.freeList.Count == 0)
+        box = new T();
+      else
+        box = this.freeList.RemoveLast();
+
+      box.Factory = this;
+      box.Initialize();
+      return box;
+    }
+  }
+}
